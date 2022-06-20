764eac4a36c785b3ecdd34d3d150a585a03b27eb
Aandrea <aandreab8@gmail.com>
2022-06-20 14:56:11 -0700

    part two done

--- api/index.js
+++ api/index.js
@@ -3,10 +3,27 @@ const apiRouter = express.Router();
 const usersRouter = require('./users');
 const postsRouter = require('./posts');
 const tagsRouter = require('./tags');
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
 
+// set `req.user` if possible
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
 
+  next();
+});
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/posts', postsRouter);
 apiRouter.use('/tags', tagsRouter);
+
+apiRouter.use((error, req, res, next) => {
+  res.send({
+    name: error.name,
+    message: error.message
+  });
+});
 
 module.exports = apiRouter;
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const PORT = 3000;
 const express = require('express');
 const server = express();
@@ -11,6 +12,18 @@ server.use('/api', apiRouter);
 
 const { client } = require('./db');
 client.connect();
+
+const jwt = require('jsonwebtoken');
+
+const token = jwt.sign({ id: 3, username: 'joshua' }, 'server secret', { expiresIn: '1h' });
+
+token; // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Impvc2h1YSIsImlhdCI6MTU4ODAyNDQwNn0.sKuQjJRrTjmr0RiDqEPJQcTliB9oMACbJmoymkjph3Q'
+
+const recoveredData = jwt.verify(token, 'server secret');
+
+recoveredData; // { id: 3, username: 'joshua', iat: 1588024406 }
+
+jwt.verify(token, 'server secret');
 
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
